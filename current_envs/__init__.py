c95ecd9feab2d13baaa8779274f67d082aacb631
nikpau <n.paulig@web.de>
2022-01-18 08:30:50 +0100

    rm unused envs

--- current_envs/__init__.py
+++ current_envs/__init__.py
@@ -1,21 +1,6 @@
 from gym.envs.registration import register 
 
 register(
-    id="MyMountainCar-v0", 
-    entry_point="current_envs.envs:MountainCar",
-)
-
-register(
-    id="Ski-v0", 
-    entry_point="current_envs.envs:Ski",
-)
-
-register(
-    id="ObstacleAvoidance-v0", 
-    entry_point="current_envs.envs:ObstacleAvoidance",
-)
-
-register(
     id="DecisionPolicy-v0", 
     entry_point="current_envs.envs:DecisionPolicy",
 )
